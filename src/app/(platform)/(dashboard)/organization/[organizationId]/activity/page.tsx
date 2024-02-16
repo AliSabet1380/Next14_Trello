2a497568289d1b26462b0bce76187441dfa011e7
AliSabet <ali8551sbt@gmail.com>
2024-02-17 00:54:59 +0330

    second commit

--- src/app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -3,11 +3,14 @@ import { Separator } from "@/components/ui/separator";
 import { Suspense } from "react";
 
 import { ActivityList } from "./_components/activity-list";
+import { checkSubscription } from "@/lib/subscription";
 
-const ActivityPage = () => {
+const ActivityPage = async () => {
+  const isPro = await checkSubscription();
+
   return (
     <div className="w-full p-10">
-      <Info />
+      <Info isPro={isPro} />
       <Separator className="bg-slate-900 my-2" />
       <Suspense fallback={<ActivityList.Skeleton />}>
         <ActivityList />
